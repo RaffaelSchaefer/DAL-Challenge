09d7cb254f3e56cca0b8d42206c1077bcbad236d
serverpaul <paul@paulruebsamen.de>
2024-06-01 17:57:36 +0200

    tesst

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -9,24 +9,6 @@
     />
 </svelte:head>
 
-<script lang="ts">
-    import PocketBase from "pocketbase";
-    
-    const pb = new PocketBase('http://10.32.6.48:8090');
-    const authData = pb.admins.authWithPassword('s20a3c@umwelt-campus.de', 'code-a-thon2024');
-
-    let vis = true;
-
-    async function fetchData() {
-        const resultList = await pb.collection('assets').getList(1, 50, {
-            filter: 'id ~ "00000068912(_.*)?"',
-        });
-    }
-
-    fetchData();
-
-</script>
-
 <style>
     @import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css';
     
@@ -67,6 +49,7 @@
         background-color: #e3dcce;
         padding-top: 10px;
         padding-bottom: 10px;
+        margin-bottom: 10px;
     }
 
     .navbar-nav { padding-right: 10px; }
@@ -94,43 +77,127 @@
         margin-right: 2%;
     }
 
+    .col {
+        border-style: solid;
+        border-color: black;
+    }
+
+    .col-8 {
+        border-style: solid;
+        border-color: black;
+    }
+
 </style>
+
+
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import PocketBase from 'pocketbase';
+
+    interface Contract {
+        id: string;
+        assetId: string;
+        assetDetails?: Asset;
+    }
+
+    interface Asset {
+        id: string;
+        name: string;
+    }
+
+    let vis = true;
+    let contracts: Contract[] = [];
+
+    const pb = new PocketBase('http://10.32.6.48:8090');
+
+    async function fetchData() {
+        try {
+            // Authentifizierung
+            const authData = await pb.admins.authWithPassword('s20a3c@umwelt-campus.de', 'code-a-thon2024');
+            
+            // Überprüfen, ob die Authentifizierung erfolgreich war
+            if (pb.authStore.isValid && pb.authStore.model) {
+                console.log('Authenticated successfully');
+                console.log('Token:', pb.authStore.token);
+                console.log('Admin ID:', pb.authStore.model.id);
+
+                // Abrufen der Vertragsdaten
+                const fetchedContracts = await pb.collection('contracts').getFullList<Contract>();
+
+                // Abrufen der zugehörigen Asset-Daten
+                for (const contract of fetchedContracts) {
+                    const asset = await pb.collection('assets').getOne<Asset>(contract.asset);
+                    contract.assetDetails = asset;
+                }
+
+                // Zuweisen der abgerufenen Vertragsdaten
+                contracts = fetchedContracts;
+
+                // Logout
+                pb.authStore.clear();
+            } else {
+                console.error('Authentication failed or auth model is null');
+            }
+        } catch (error) {
+            console.error('Fehler beim Abrufen der Daten:', error);
+        }
+    }
+
+    onMount(() => {
+        fetchData();
+    });
+</script>
 
 <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
-        <a href="#" class="navbar-brand">
+        <a href="/" class="navbar-brand">
             <img src="../icons/RGB/Logo_DAL_RGB.png" height="34" alt="DAL">
         </a>
         <div class="navbar-nav">
             {#if vis}
-            <a href="#" class="nav-item nav-link">Konto</a>
-            <a href="#" class="nav-item nav-link">Postfach</a>
+            <a href="/" class="nav-item nav-link">Konto</a>
+            <a href="/" class="nav-item nav-link">Postfach</a>
             <div class="dropdown dd">
                 <button class="dropbtn nav-item nav-link">Verträge</button>
                 <div class="dropdown-content">
-                  <a href="#">Link 1</a>
-                  <a href="#">Link 2</a>
-                  <a href="#">Link 3</a>
+                  <a href="/">Link 1</a>
+                  <a href="/">Link 2</a>
+                  <a href="/">Link 3</a>
                 </div>
             </div>  
             <div class="dropdown dd">
                 <button class="dropbtn nav-item nav-link">Assets</button>
                 <div class="dropdown-content">
-                  <a href="#">Link A</a>
-                  <a href="#">Link B</a>
-                  <a href="#">Link C</a>
+                  <a href="/">Link A</a>
+                  <a href="/">Link B</a>
+                  <a href="/">Link C</a>
                 </div>
             </div>                     
             {/if}
         </div>
         <div class="navbar-nav ms-auto">
             {#if vis}
-                <a href="#" class="nav-item nav-link">Logout</a>
+                <a href="/" class="nav-item nav-link">Logout</a>
             {/if}
             {#if !vis}
-                <a href="#" class="nav-item nav-link">Login</a>
+                <a href="/" class="nav-item nav-link">Login</a>
             {/if}
-            <a href="#" class="nav-item nav-link">Einstellungen </a>
+            <a href="/" class="nav-item nav-link">Einstellungen </a>
         </div>
     </div>
 </nav>
+
+<div class="container">
+    {#each contracts as contract}
+        <div class="row">
+            <div class="col">
+                <p>Contract ID: {contract.id}</p>
+            </div>
+            <div class="col-8">
+                <p>Asset ID: {contract.assetDetails.id}</p>
+                <p>Asset Name: {contract.assetDetails.type}</p>
+            </div>
+        </div>
+    {/each}
+</div>
+
